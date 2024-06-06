da67f3b73f3fb9c4ccb2f9d0c7816b1844f11e97
Niyazi <59951832+zlobendog@users.noreply.github.com>
2024-06-06 14:49:03 +0400

    Test added

---
+++ tests/test_encryption_decryption.py
@@ -0,0 +1,19 @@
+import polars as pl
+from polars_encryption import encrypt, decrypt
+
+
+def test_validity_of_decryption():
+    df = pl.DataFrame({
+        "plaintext": ["Hello, world!", "Polars is fast!", "Encrypt me!"]
+    })
+
+    # Define the encryption key and nonce
+    key = b"an example very very secret key."
+    nonce = b"unique nonce"  # 12 bytes (96 bits)
+
+    # Encrypt the plaintext column
+    df_encrypted = df.with_columns(
+    encrypt(pl.col("plaintext"), key=key, nonce=nonce).alias("ciphertext")).with_columns(decrypt(pl.col("ciphertext"), key=key, nonce=nonce).alias("decrypted"))
+
+
+    assert df["plaintext"].series_equal(df_encrypted["decrypted"]), "Decrypted values do not match the original plaintext values."
